f072c59b65b2a32265fc5c9b4e5d0ee3d037c64f
AnaValca <avc92j@gmail.com>
2020-06-25 12:58:34 +0200

    changes map await

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import '../stylesheets/App.scss';
-import Maps from './Map'
+import Maps from './Map';
+;
+
+const geoPosition = {
+  longitude: '',
+  latitude: '',
+}
 
 function App() {
+
+   const [{longitude, latitude}, setLocalPosition] =  useState(geoPosition)
+  // useEffect(() => {
+    // create map
+    navigator.geolocation.getCurrentPosition(function(position){
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    setLocalPosition({
+        latitude : position.coords.latitude,
+        longitude : position.coords.longitude,
+        
+    })
+    
+      })
+       let position = [latitude, longitude];
+  
   return (
     <div className="App">
-      <Maps />
+      
+      
+      <Maps latitude={latitude} longitude={longitude}/>
+   
     </div>
   );
+  
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,23 +1,28 @@
-import React, { useEffect } from 'react';
-import L from 'leaflet';
+import React, {useEffect} from 'react';
+import  L  from 'leaflet';
 // import { TileLayer, Marker, Popup} from 'react-leaflet';
 
-function Maps() {
-    useEffect(() => {
-      // create map
-      L.map('map', {
-        center: [49.8419, 24.0315],
-        zoom: 16,
-        layers: [
-          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution:
-              '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          }),
-        ]
-      });
-    }, []);
+// 
+
+
+ function Maps({latitude,longitude}) {
   
-    return <div id="map"></div>
+  console.log(latitude)
+  console.log(longitude)
+   useEffect( ()  => {
+   L.map('map', { 
+      center: [latitude, longitude],
+      zoom: 16,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }),
+      ]
+    });console.log(latitude)
+  }, []);
+
+    return  <div id="map"></div>
   }
 
   export default Maps;
